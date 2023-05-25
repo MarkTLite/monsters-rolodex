bf77362d44b63628d21d58d2ee976a7fc8f6aad8
MarkTLite <profturyahabwa@gmail.com>
2023-05-25 13:52:25 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import CardList from './components/card-list/card-list-component';
 import SearchBox from './components/search-box/search-box-component';
